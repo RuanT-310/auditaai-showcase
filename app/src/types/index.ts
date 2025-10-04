9355a3d4de875ff8ee87af58735b071d865a29f0
Ruan Rolim <ruan.rolim.137@gmail.com>
2025-10-04 16:12:14 -0300

    feat: setup project

---
+++ app/src/types/index.ts
@@ -0,0 +1,23 @@
+// --- Interfaces de Dados ---
+interface Award {
+  title: string;
+  event: string;
+  color: string;
+}
+
+interface Feature {
+  title: string;
+  description: string;
+  iconPath: string;
+  iconColor: string;
+}
+
+interface Step {
+  title: string;
+  description: string;
+}
+
+interface Partner {
+  name: string;
+  description: string;
+}
